91e73c29f4b5abff1327a013b66605da7813e2de
ruandqm <ruandqm@gmail.com>
2022-12-13 19:38:00 -0300

    twoSums fix

--- 13.12.2022/index.js
+++ 13.12.2022/index.js
@@ -1,7 +1,9 @@
 function twoSums(nums, target) {
     for (let i = 0; i < nums.length; i++) {
-        if ((nums[i] + nums[i + 1]) === target) {
-            return ([i, i + 1])
+        for (let j = 1; j < nums.length + 1; j++) {
+            if ((nums[i] + nums[j]) === target) {
+                return ([i, j])
+            }
         }
     }
 }
