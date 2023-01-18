00e1470e71eb7ebae113cf776825530d7ab80f36
ruandqm <ruandqm@gmail.com>
2023-01-18 19:33:19 -0300

    18.01.2023 (unfinished)

--- 10.01.2023/index.js
+++ 10.01.2023/index.js
@@ -1,48 +1,32 @@
 const map = {
-    two: ['a', 'b', 'c'],
-    three: ['d', 'e', 'f'],
-    four: ['g', 'h', 'i'],
-    five: ['j', 'k', 'l'],
-    six: ['m', 'n', 'o'],
-    seven: ['p', 'q', 'r', 's'],
-    eight: ['t', 'u', 'v'],
-    nine: ['w', 'x', 'y', 'z'],
+    '2': ['a', 'b', 'c'],
+    '3': ['d', 'e', 'f'],
+    '4': ['g', 'h', 'i'],
+    '5': ['j', 'k', 'l'],
+    '6': ['m', 'n', 'o'],
+    '7': ['p', 'q', 'r', 's'],
+    '8': ['t', 'u', 'v'],
+    '9': ['w', 'x', 'y', 'z'],
 }
 
 function letters(input) {
     let numbers = input.split('')
+    let output = []
+    let outputLength = 0
+    let mapResult = []
 
     for (let i = 0; i < numbers.length; i++) {
-        let actNumber
-
-        switch (numbers[i]) {
-            case 1:
-                actNumber = 1
-                break
-            case 2:
-                actNumber = 2
-                break
-            case 3:
-                actNumber = 3
-                break
-            case 4:
-                actNumber = 4
-                break
-            case 5:
-                actNumber = 5
-                break
-            case 6:
-                actNumber = 6
-                break
-            case 7:
-                actNumber = 7
-                break
-            case 8:
-                actNumber = 8
-                break
-            case 9:
-                actNumber = 9
-                break
-        }
+        output.push(map[numbers[i]])
     }
+
+    for (let i = 0; i < output.length; i++) {
+        outputLength++
+    }
+    for (let i = 0; i < outputLength; i++) {
+
+    }
+
+    console.log(output)
 }
+
+letters('23')
---
+++ 18.01.2023/index.js
@@ -0,0 +1,12 @@
+function driver(data) {
+    const firstName = data[0]
+    const secondName = data[1]
+    const lastName = data[2]
+    const birth = data[3]
+    const genre = data[4]
+
+
+}
+
+const driverData = ['John', 'James', 'Smith', '01-Jan-2000', 'M']
+driver(driverData)
