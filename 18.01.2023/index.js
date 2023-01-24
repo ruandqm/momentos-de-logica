5bf0ac4511e96ccf018016e57149e13ed375a75a
ruandqm <ruandqm@gmail.com>
2023-01-24 19:32:09 -0300

    24.01.2023 (unfinished)

--- 18.01.2023/index.js
+++ 18.01.2023/index.js
@@ -1,11 +1,17 @@
 function driver(data) {
     const firstName = data[0]
     const secondName = data[1]
-    const lastName = data[2]
+    const lastName = data[2].toUpperCase()
     const birth = data[3]
     const genre = data[4]
 
-
+    let first
+    lastName.length >= 5 ? first = lastName.substr(0, 5) : first = lastName.substr(0, lastName.length)
+    while (first.length < 5)
+        first = first + '9'
+    console.log(first)
+    console.log(birth[9])
+    genre === 'F' ? console.log('')
 }
 
 const driverData = ['John', 'James', 'Smith', '01-Jan-2000', 'M']
